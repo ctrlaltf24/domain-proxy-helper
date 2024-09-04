bb5eba37fe36cdcab825f921fc1f9ee884347dcc
Nathan Shaaban <86252985+ctrlaltf24@users.noreply.github.com>
2024-09-03 21:03:49 -0700

    feat: switch to supporting per-domain

--- background.js
+++ background.js
@@ -1,147 +1,70 @@
-let settings = {};
+const browserSettingsKey = 'com.github.ctrlaltf24.browser-proxy-switcher.domains';
 
-const blocked = {
-    type: 'socks',
-    host: 'block-proxy.localhost',
-    port: 1,
-    failoverTimeout: 1,
-    username: 'nonexistent user',
-    password: 'random password',
-    proxyDNS: true
-};
 
-function getStoreIdProxy(storeId) {
-    if (settings[storeId] && settings[storeId].proxies && settings[storeId].proxies.length) {
-        return settings[storeId].proxies[0];
-    }
-    if (settings.default && settings.default.proxies && settings.default.proxies.length) {
-        return settings.default.proxies[0];
-    }
-    return '$block';
-}
+// Format of settings is:
+// [
+//     {
+//         // UUID v4
+//         "id": "00000000-0000-0000-0000-000000000000",
+//         // Domain name
+//         "hostname": "firefox-default",
+//         // Proxy settings
+//         "proxy": {
+//             "type": "socks4",
+//             "username": "user",
+//             "password": "password",
+//             "host": "",
+//             "port": "1080",
+//             "proxyDNS": true
+//         },
+//         // Proxy string (used for easy display)
+//         "proxyString": "socks4://localhost:1080"
+//     }
+// ]
+let settings = [];
 
 function handleProxifiedRequest(requestInfo) {
-    let storeId = requestInfo.cookieStoreId;
-    if (!storeId) {
-        if (requestInfo.incognito) {
-            storeId = 'firefox-private';
-        } else if (requestInfo.tabId !== -1) {
-            storeId = 'firefox-default';
-        } else {
-            storeId = 'firefox-unknown';
+    let requestHostname = new URL(requestInfo.url).hostname;
+    // Go through the list of domains and see if the request matches any of them
+    let matchedSetting = null;
+    for (let index in settings) {
+        let setting = settings[index];
+        if (requestHostname === setting.hostname || requestHostname.endsWith('.' + setting.hostname)) {
+            matchedSetting = setting;
+            break;
         }
     }
-    else if (storeId === 'firefox-default' && requestInfo.tabId === -1) {
-        storeId = 'firefox-unknown';
-    }
-    let proxy = getStoreIdProxy(storeId);
-    /*
-    *   @see https://bugzilla.mozilla.org/show_bug.cgi?id=1750572
-    *   @see https://bugzilla.mozilla.org/show_bug.cgi?id=1750561
-    */
-    if (proxy === '$firefox') {
+    if (!matchedSetting) {
+        // If no match was found then don't change the proxy
         return;
     }
-    if (proxy === '$direct') {
-        return null;
-    }
-    if (proxy === '$block' || !proxy || !proxy.type) {
-        return [blocked, null];
-    }
-    return [{...proxy, connectionIsolationKey: '' + storeId}, null];
+
+    // XXX: Not sure if this will respect the proxy DNS setting.
+    // May need to implement a custom DNS resolver
+
+    // Only return the one proxy setting that matches the domain
+    return {
+        ...matchedSetting.proxy,
+        connectionIsolationKey: browserSettingsKey + "." + matchedSetting.id
+    };
 }
-
-async function updateIcon(tabInfo) {
-    if (tabInfo.id === -1) {
-        return;
-    }
-    try {
-        let storeId = tabInfo.cookieStoreId;
-        if (!storeId) {
-            if (tabInfo.incognito) {
-                storeId = 'firefox-private';
-            } else {
-                storeId = 'firefox-default';
-            }
-        }
-
-        let result = getStoreIdProxy(storeId),
-            icon = 'icons/blocked.svg',
-            title = 'Blocked';
-        if (result && result !== '$block') {
-            if (result === '$direct') {
-                icon = 'icons/unlocked.svg';
-                title = 'Direct Connection';
-            } else if (result === '$firefox') {
-                icon = 'icons/warning.svg';
-                title = 'Firefox connection settings';
-            } else if (result.type && result.type === 'socks') {
-                if (result.host && result.host.match(/^[a-z]{2}\d{1,3}-wg\.socks5\.mullvad\.net$/i)) {
-                    icon = 'icons/flags/' + result.host.toUpperCase().substr(0, 2) + '.png';
-                    title = result.host;
-                }
-                else {
-                    icon = 'icons/locked.svg';
-                    title = result.host;
-                }
-            }
-        }
-        await browser.browserAction.setIcon({
-            tabId: tabInfo.id, path: icon
-        });
-        await browser.browserAction.setTitle({
-            tabId: tabInfo.id, title: title
-        });
-
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-
-// Get the stored list and update icon
-browser.storage.local.get((data) => {
-    if (data) {
-        settings = data;
-    }
-
-    browser.tabs.query({active: true}).then((tabs) => {
-        if (tabs.length) {
-            return updateIcon(tabs[0]);
-        }
-    });
-});
 
 // Listen for a request to open a webpage
-browser.proxy.onRequest.addListener(handleProxifiedRequest, {urls: ["<all_urls>"]});
+browser.proxy.onRequest.addListener(handleProxifiedRequest, { urls: ["<all_urls>"] });
 
 // Log any errors from the proxy script
 browser.proxy.onError.addListener(error => {
     console.error(`Proxy error: ${error.message}`);
 });
 
-//open preferences on icon click
-browser.browserAction.onClicked.addListener(async () => {
-    await browser.runtime.openOptionsPage();
-});
-
-//update icon on tab activation
-browser.tabs.onActivated.addListener(async (info) => {
-    let tabInfo = await browser.tabs.get(info.tabId);
-    await updateIcon(tabInfo);
-});
-
-//update icon on tab update
-browser.tabs.onUpdated.addListener(async (tabId, changeInfo, tabInfo) => {
-    await updateIcon(tabInfo);
-});
-
-// Listen for changes in extenstion storage
+// Listen for changes in extension storage
 browser.storage.onChanged.addListener((changes, area) => {
     if (area === 'local') {
         let changedItems = Object.keys(changes);
         for (let item of changedItems) {
-            settings[item] = changes[item].newValue;
+            if (item === browserSettingsKey) {
+                settings = changes[item].newValue;
+            }
         }
     }
 });
--- options/options.js
+++ options/options.js
@@ -2,127 +2,98 @@ const template = document.querySelector('#proxyInput');
 const containersList = document.querySelector('#containers');
 const commonList = document.querySelector('#common');
 
-
-const connectionTypes = {
-    '': [],
-    '$firefox': ['$firefox'],
-    '$direct': ['$direct'],
-    '$block': ['$block']
-};
+const browserSettingsKey = 'com.github.ctrlaltf24.browser-proxy-switcher.domains';
 
 let settings = {};
 
-//right now only one proxy
-function getProxiesFromInput(text) {
+function getProxyFromInput(text) {
     text = text.trim();
     if (text === '') {
-        return [];
-    }
-    //mullvad aliases
-    if (text.toLowerCase().match(/^[a-z]{2}\d{1,3}$/i)) {
-        return [{
-            type: 'socks',
-            host: text.toLowerCase() + '-wg.socks5.mullvad.net',
-            port: 1080,
-            proxyDNS: true
-        }];
+        return {};
     }
     const proxyRegexp = /(?<type>(socks4?)):\/\/(\b(?<username>\w+):(?<password>\w+)@)?(?<host>((?:\d{1,3}\.){3}\d{1,3}\b)|(\b([\w.-]+)+))(:(?<port>\d+))?/;
     const matches = proxyRegexp.exec(text);
     if (!matches) {
-        return ['$block'];
+        return {};
     }
-    return [{...matches.groups, proxyDNS: true}];
+    return { ...matches.groups, proxyDNS: true };
 }
 
 function storeSettings(e) {
     const row = e.target.closest('[data-identity-id]');
-    const storeId = row.getAttribute('data-identity-id');
-    if (storeId) {
-        const select = row.querySelector('select.connectionType');
-        let result = {};
+    const id = row.getAttribute('data-identity-id');
+    if (id) {
+        const proxyInput = row.querySelector('input.userContext-proxy');
+        const domainInput = row.querySelector('input.userContent-domain');
+        if (!proxyInput || !domainInput || proxyInput.value === '' || domainInput.value === '') {
+            return;
+        }
+        let result = {
+            "id": id,
+            "hostname": domainInput.value,
+            "proxy": getProxyFromInput(proxyInput.value),
+            "proxyString": proxyInput.value
+        };
 
-        if (connectionTypes[select.value]) {
-            result = {
-                input: select.value,
-                proxies: connectionTypes[select.value]
+        let newSettingIndex = -1;
+        const originalSettings = settings[browserSettingsKey] || [];
+        for (let i = 0; i < originalSettings.length; i++) {
+            if (originalSettings[i].id === id) {
+                newSettingIndex = i;
+                break;
             }
         }
-        else {
-            const input = row.querySelector('input.userContext-proxy');
-            result = {
-                input: input.value,
-                proxies: getProxiesFromInput(input.value)
-            }
+        let settingsToStore = originalSettings;
+        if (newSettingIndex === -1) {
+            settingsToStore = [...originalSettings, result];
+        } else {
+            settingsToStore[newSettingIndex] = result;
         }
-
+        
         browser.storage.local.set({
-            [storeId]: result
+            [browserSettingsKey]: settingsToStore
         });
 
     }
 }
 
-function showInfo(select) {
-    select.parentElement.querySelectorAll('[data-value]').forEach(el => {
-        if (el.getAttribute('data-value') === select.value) {
-            el.removeAttribute('hidden');
-        }
-        else {
-            el.setAttribute('hidden', '');
-        }
-    });
-}
 
-function printContainerRow(identity, list) {
+function printContainerRow(domain, list) {
     const row = template.content.cloneNode(true);
     const div = row.querySelector('div');
-    div.className = 'identity-icon-' + identity.icon + ' identity-color-' + identity.color;
-    div.setAttribute('data-identity-id', identity.cookieStoreId);
+    div.setAttribute('data-identity-id', domain.id);
 
-    row.querySelector('.userContext-label').textContent = identity.name;
+    const domainInput = row.querySelector('input.userContent-domain')
+    domainInput.value = domain.hostname;
 
-    const select = row.querySelector('select.connectionType');
-    const input = row.querySelector('input.userContext-proxy');
-    if (identity.cookieStoreId === 'default') {
-        select.querySelector('option').disabled = true;
-    }
+    const proxyInput = row.querySelector('input.userContext-proxy');
+    proxyInput.value = domain.proxyString;
 
-    if (settings[identity.cookieStoreId] && settings[identity.cookieStoreId].input) {
-        let idx = Object.keys(connectionTypes).indexOf(settings[identity.cookieStoreId].input);
-        if (idx > 0) {
-            input.value = '';
-            select.selectedIndex = idx;
-        }
-        else {
-            input.value = settings[identity.cookieStoreId].input;
-            select.selectedIndex = 4;
-        }
-    }
-    else {
-        input.value = '';
-        select.selectedIndex = identity.cookieStoreId === 'default' ? 3 : 0;
-    }
-    showInfo(select);
-    input.addEventListener('change', storeSettings);
-    select.addEventListener('change', storeSettings);
-    select.addEventListener('change', event => { showInfo(event.target) });
+    // showInfo(div);
+    div.removeAttribute('hidden');
+    proxyInput.addEventListener('change', storeSettings);
+    domainInput.addEventListener('change', storeSettings);
+
     list.appendChild(row);
 }
 
 async function setupContainerFields() {
     settings = await browser.storage.local.get();
-    const identities = await browser.contextualIdentities.query({});
-    const incognito = await browser.extension.isAllowedIncognitoAccess();
-    printContainerRow({name: "Default", icon: "default", color: "default", cookieStoreId: "default"}, commonList);
-    printContainerRow({name: "Unknown", icon: "unknown", color: "red", cookieStoreId: "firefox-unknown"}, commonList);
-    printContainerRow({name: "No Container", icon: "nocontainer", color: "nocontainer", cookieStoreId: "firefox-default"}, commonList);
-    if (incognito) {
-        printContainerRow({name: "Private", icon: "private", color: "private", cookieStoreId: "firefox-private"}, commonList);
+
+    // See background.js for the structure of the domains object
+    let domains = settings[browserSettingsKey] || [];
+
+    for (const domain of domains) {
+        printContainerRow(domain, containersList);
     }
-    for (const identity of identities) {
-        printContainerRow(identity, containersList);
-    }
+
+    // Print one extra row
+    printContainerRow({
+        "id": domains.length + 1,
+        "hostname": "",
+        "proxy": {},
+        "proxyString": ""
+    }, containersList);
 }
 
 setupContainerFields().catch(e => { console.log(e) });
